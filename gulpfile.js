b42311dfe75cb83b18ee271622383b29b4a7a6ae
Kaloyan Marinov <kalmar98softuni@gmail.com>
2019-04-08 16:42:53 +0300

    Implemented SASS/SCSS.

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,57 @@
 "use strict";
 
 let gulp = require("gulp"),
+	sass = require("gulp-sass"),
 	autoprefixer = require("gulp-autoprefixer"),
 	exec = require("gulp-exec"),
 	browserSync = require('browser-sync').create(),
 	cp = require("child_process");
 ;
 
-gulp.task("css", function() {
-	return gulp.src( '_assets/css/**/*.css' )
-		.pipe( autoprefixer() )
-		.pipe( gulp.dest( './docs/css/' ) )
-		.pipe( browserSync.stream({ match: '**/*.css' }))
-	;
+sass.compiler = require("node-sass");
+
+gulp.task("scss", function () {
+	return gulp.src('_assets/scss/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer())
+		.pipe(gulp.dest('./docs/css/'))
+		.pipe(browserSync.stream({ match: '**/*.css' }))
+		;
 });
 
-gulp.task("js", function() {
-	return gulp.src( '_assets/js/**/*.js' )
-		.pipe( gulp.dest( './docs/js/' ) )
-		.pipe( browserSync.stream({ match: '**/*.js' }))
-	;
+gulp.task("js", function () {
+	return gulp.src('_assets/js/**/*.js')
+		.pipe(gulp.dest('./docs/js/'))
+		.pipe(browserSync.stream({ match: '**/*.js' }))
+		;
 });
 
-gulp.task("img", function() {
-	return gulp.src( '_assets/img/**/*.jpg' )
-		.pipe( gulp.dest( './docs/img/' ) )
-		.pipe( browserSync.stream({ match: '**/*.jpg' }))
-	;
+gulp.task("img", function () {
+	return gulp.src('_assets/img/**/*.jpg')
+		.pipe(gulp.dest('./docs/img/'))
+		.pipe(browserSync.stream({ match: '**/*.jpg' }))
+		;
 });
 
 // Jekyll
-gulp.task("jekyll", function() {
+gulp.task("jekyll", function () {
 	return cp.exec("jekyll build");
 	//return cp.spawn("bundle", ["jekyll", "build"], { stdio: "inherit" });
 });
 
-gulp.task("watch", function() {
+gulp.task("watch", function () {
 
 	browserSync.init({
 		server: {
-            baseDir: "./docs/"
+			baseDir: "./docs/"
 		}
 	});
 
-	gulp.watch( '_assets/css/**/*.css', gulp.series('css') );
+	gulp.watch('_assets/scss/**/*.scss', gulp.series('scss'));
 
-	gulp.watch( '_assets/js/**/*.js', gulp.series('js') );
+	gulp.watch('_assets/js/**/*.js', gulp.series('js'));
 
-	gulp.watch( '_assets/img/**/*.jpg', gulp.series('img') );
+	gulp.watch('_assets/img/**/*.jpg', gulp.series('img'));
 
 	gulp.watch(
 		[
@@ -58,12 +62,12 @@ gulp.task("watch", function() {
 			"./views/*.html",
 			//"./_posts/**/*.*"
 		],
-	).on('change', gulp.series('jekyll', 'css', 'js', 'img'));
+	).on('change', gulp.series('jekyll', 'scss', 'js', 'img'));
 
-	gulp.watch( 'docs/**/*.html' ).on('change', browserSync.reload );
-	gulp.watch( 'docs/**/*.js' ).on('change', browserSync.reload );
-	gulp.watch( 'docs/**/*.jpg' ).on('change', browserSync.reload );
+	gulp.watch('docs/**/*.html').on('change', browserSync.reload);
+	gulp.watch('docs/**/*.js').on('change', browserSync.reload);
+	gulp.watch('docs/**/*.jpg').on('change', browserSync.reload);
 
 });
 
-gulp.task("default", gulp.series('jekyll', 'css', 'js', 'img', 'watch'));
+gulp.task("default", gulp.series('jekyll', 'scss', 'js', 'img', 'watch'));
